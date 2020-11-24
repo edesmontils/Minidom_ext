2c7133a23e8a8a189b3082572f719997c97001cf
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2020-11-24 22:51:51 +0100

    dd

--- README.md
+++ README.md
@@ -2,9 +2,25 @@
 
 ## Short presentation
 
-Functions to improve xml.dom.minidom tools in Python :
-- purgeDOM : to suppress text nodes with only separators and comments
-- enrichXML : to add attributes with a default value
+Functions to improve xml.dom.minidom tools in Python.
+class DocumentCompanion :
+-	def __init__(self, doc = None) : build a DOM DocumentCompanion
+-	def parse(self, file, validate = False): load en XML file
+-	def enrichXML(self) : adds default attribute and detects IDs
+-	def getElementsByTagName(self, name) : DOM getElementsByTagName
+-	def getElementById(self, id) : retrieve an element by its ID (only if "enrichXML" or "validate" is done)
+-	def toLighter(self, del_spaces = True, del_comments = True, del_pi = True) : to suppress text nodes (with only separators), processing instructions and/or comments
+-	def validate(self) : validate the XML according its DTD (enrich it too)
+
+## Exemple
+
+```
+cine = DocumentCompanion()
+cine.parse("semaine10.xml", True)
+print(cine.doc.toxml())
+print(cine.getElementById('Ka'))
+print(cine.toLighter().toxml())
+```
 
 
 ## Links
--- minidom_ext.py
+++ minidom_ext.py
@@ -1,91 +1,135 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+7
 from xml.dom.minidom import Node, Element, parse
 import re
+from lxml import etree  # http://lxml.de/index.html#documentation
 
+class DocumentCompanion :
+	def __init__(self, doc = None) :
+		self.doc = doc
+		self.lid = dict()
+		if doc is not None :
+			self.enrichXML()
 
-def purgeDOM(no, del_spaces = True, del_comments = True, del_pi = True) :
-	if no.nodeType == Node.ELEMENT_NODE :
-		toDel = []
-		for n in no.childNodes :
-			if del_spaces and n.nodeType == Node.TEXT_NODE and n.data.strip('\t \n') == '' :
-				toDel.append(n)
-			elif del_comments and n.nodeType == Node.COMMENT_NODE :
-				toDel.append(n)
-			elif del_pi and n.nodeType == Node.PROCESSING_INSTRUCTION_NODE :
-				toDel.append(n)
-			elif n.nodeType == Node.ELEMENT_NODE :
-				purgeDOM(n,del_spaces,del_comments, del_pi)
-		for n in toDel :
-			no.removeChild(n)
-	elif no.nodeType == Node.DOCUMENT_NODE :
-		for n in no.childNodes :
-			purgeDOM(n,del_spaces,del_comments, del_pi)
-	elif no.nodeType == Node.DOCUMENT_TYPE_NODE :
-		pass
-	else :
-		pass
-	return no
+	def parse(self, file, validate = False):
+		self.doc = parse(file)
+		if validate :
+			self.validate()
 
-def getDTD(file) :
-	f = open(file,'r')
-	dtd = f.read()
-	f.close()
-	return dtd
+	def enrichXML(self) :
+		self.lid = dict()
+		dtdFile = self.doc.doctype.systemId
+		if dtdFile is not None :
+			le = self.extractDTD(dtdFile)
+			self.enrichNode(self.doc.documentElement, le)
 
-def extractDTD(file) :
+	def getElementsByTagName(self, name) :
+		return self.doc.getElementsByTagName(name)
 
-	el = re.compile(r'<!ELEMENT (?P<elementname>[\w\-\:\_]+) (?P<description>.*)\s*>')
-	att = re.compile(r'<!ATTLIST (?P<elementname>[\w\-\:\_]+) (?P<attributs>.*)\s*>')
-	att2 = re.compile(r'(?P<attname>[\w\-\:\_]+) (?P<def>.*?) (?P<status>#[\w\-\:\_]+|[\"\'].*?[\"\'])')
-	comment = re.compile(r'<!-- \.*? -->')
+	def getElementById(self, id) :
+		if id in self.lid.keys() :
+			return self.lid[id]
+		else :
+			return None
 
-	dtd = getDTD(file).replace('\n',' ').replace('\t',' ')
-	cp = re.compile(r'<.*?>')
-	liste_elem = dict()
-	for item in cp.findall(dtd) :
-		cmnt = comment.match(item)
-		if cmnt is not None :
+	def toLighter(self, del_spaces = True, del_comments = True, del_pi = True) :
+		return self.purgeDOM(self.doc, del_spaces, del_comments, del_pi)
+
+	def validate(self) :
+		parser = etree.XMLParser(recover=True, strip_cdata=True)
+		tree = etree.XML(self.doc.toxml(), parser)
+		dtdFile = self.doc.doctype.systemId
+		if dtdFile is not None :
+			dtd = etree.DTD(dtdFile)
+			if dtd.validate(tree) :
+				self.enrichXML()
+				return True
+			else :
+				print(dtd.error_log.filter_from_errors()[0])
+				return False
+		else:
+			return True
+
+	#####################################
+	########## private methods ##########
+	#####################################
+
+	def purgeDOM(self, no, del_spaces, del_comments, del_pi) :
+		if no.nodeType in [Node.ELEMENT_NODE, Node.DOCUMENT_NODE] :
+			toDel = []
+			for n in no.childNodes :
+				if del_spaces and n.nodeType == Node.TEXT_NODE and n.data.strip('\t \n') == '' :
+					toDel.append(n)
+				elif del_comments and n.nodeType == Node.COMMENT_NODE :
+					toDel.append(n)
+				elif del_pi and n.nodeType == Node.PROCESSING_INSTRUCTION_NODE :
+					toDel.append(n)
+				elif n.nodeType == Node.ELEMENT_NODE :
+					self.purgeDOM(n,del_spaces,del_comments, del_pi)
+			for n in toDel :
+				no.removeChild(n)
+		elif no.nodeType == Node.DOCUMENT_TYPE_NODE :
 			pass
 		else :
-			grp = el.match(item)
-			if grp is not None :
-				nomElem = grp.group('elementname').strip('\t \n')
-				liste_elem[nomElem] = dict()
-			else :
-				grp = att.match(item)
-				if grp is not None :
-					nomElem = grp.group('elementname')
-					for (nom, definition, status) in att2.findall(grp.group('attributs')) :
-						nomAtt = nom.strip('\t \n')
-						definition = definition.strip('\t \n')
-						status = status.strip('\t \n')
-						liste_elem[nomElem][nomAtt] = (definition, status.replace("'",'').replace('"',''))
-	return liste_elem
+			pass
+		return no
 
-def enrichNode(node, le) :
-	if node.nodeType == Node.ELEMENT_NODE :
-		la = le[node.tagName]
-		for (att, (definition, status)) in la.items() :
-			if node.hasAttribute(att) :
+	def getDTD(self, file) :
+		f = open(file,'r')
+		dtd = f.read()
+		f.close()
+		return dtd
+
+	def extractDTD(self, file) :
+
+		el = re.compile(r'<!ELEMENT (?P<elementname>[\w\-\:\_]+) (?P<description>.*)\s*>')
+		att = re.compile(r'<!ATTLIST (?P<elementname>[\w\-\:\_]+) (?P<attributs>.*)\s*>')
+		att2 = re.compile(r'(?P<attname>[\w\-\:\_]+) (?P<def>.*?) (?P<status>#[\w\-\:\_]+|[\"\'].*?[\"\'])')
+		comment = re.compile(r'<!-- \.*? -->')
+
+		dtd = self.getDTD(file).replace('\n',' ').replace('\t',' ')
+		cp = re.compile(r'<.*?>')
+		liste_elem = dict()
+		for item in cp.findall(dtd) :
+			cmnt = comment.match(item)
+			if cmnt is not None :
 				pass
 			else :
-				if '#' not in status :
-					node.setAttribute(att,status) 
-		for n in node.childNodes :
-			enrichNode(n,le)
+				grp = el.match(item)
+				if grp is not None :
+					nomElem = grp.group('elementname').strip('\t \n')
+					liste_elem[nomElem] = dict()
+				else :
+					grp = att.match(item)
+					if grp is not None :
+						nomElem = grp.group('elementname')
+						for (nom, definition, status) in att2.findall(grp.group('attributs')) :
+							nomAtt = nom.strip('\t \n')
+							definition = definition.strip('\t \n')
+							status = status.strip('\t \n')
+							liste_elem[nomElem][nomAtt] = (definition, status.replace("'",'').replace('"',''))
+		return liste_elem
 
-def enrichXML(doc) :
-	dtdFile = doc.doctype.systemId
-	if dtdFile is not None :
-		#print(dtdFile)
-		le = extractDTD(dtdFile)
-		enrichNode(doc.documentElement, le)
+	def enrichNode(self, node, le) :
+		if node.nodeType == Node.ELEMENT_NODE :
+			la = le[node.tagName]
+			for (att, (definition, status)) in la.items() :
+				if definition == 'ID' :
+					nid = node.getAttribute(att)
+					self.lid[nid] = node
+				if node.hasAttribute(att) :
+					pass
+				else :
+					if '#' not in status :
+						node.setAttribute(att,status) 
+			for n in node.childNodes :
+				self.enrichNode(n,le)
 
 
 if ( __name__ == "__main__"):
-	doc = parse("semaine10.xml")
-	#purgeDOM(doc)
-
-	enrichXML(doc)
-	print(doc.toxml())
+	cine = DocumentCompanion()
+	cine.parse("semaine10.xml", True)
+	print(cine.doc.toxml())
+	print(cine.getElementById('Ka'))
+	print(cine.toLighter().toxml())
